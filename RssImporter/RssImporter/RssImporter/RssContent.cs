eea9210fd9e417c37bf2c677dbf31ef3b827c6e3
JaikeMCD <41348865+JaikeMCD@users.noreply.github.com>
2018-09-12 16:01:49 +1000

    week 7 changes

---
+++ RssImporter/RssImporter/RssImporter/RssContent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace RssImporter
+{
+    public class RssContent
+    {
+
+        public string title { get; set; }
+        public string description { get; set; }
+        public string link { get; set; }
+        public string startDate { get; set; }
+        public string startDateTime { get; set; }
+        public string endDateTime { get; set; }
+        public bool allDay { get; set; }
+        public string cost { get; set; }
+        public string age { get; set; }
+        public string venue { get; set; }
+        public string venueAddress { get; set; }
+
+    }
+}
